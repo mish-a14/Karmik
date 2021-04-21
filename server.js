67d8e051043b90b19904602d06962e435af19247
Dor <dorzairi@ymail.com>
2021-04-21 18:15:41 -0400

    fixed typos

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const logger = require("morgan");
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json);
+app.use(express.json());
 
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
