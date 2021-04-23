1c18a962f93b05bfe6f21ada684e9f419269d199
Dor <dorzairi@ymail.com>
2021-04-22 22:52:49 -0400

    added some functions

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,11 +3,39 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    user: null
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+    error: ""
   };
 
-  setUserInState = incomingUserData => {
-    this.setState({ user: incomingUserData });
+  handleSubmit = async evt => {
+    evt.preventDefault();
+    try {
+      // 1. POST our new user info to the server
+      const fetchResponse = await fetch("/api/users/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password
+        })
+      });
+
+      // 2. Check "fetchResponse.ok". False means status code was 4xx from the server/controller action
+      if (!fetchResponse.ok) throw new Error("Fetch failed - Bad request");
+
+      let token = await fetchResponse.json(); // 3. decode fetch response to get jwt from srv
+      localStorage.setItem("token", token); // 4. Stick token into localStorage
+
+      const userDoc = JSON.parse(atob(token.split(".")[1])).user; // 5. Decode the token + put user document into state
+      this.props.setUserInState(userDoc);
+    } catch (err) {
+      console.log("SignupForm error", err);
+      this.setState({ error: "Sign Up Failed - Try Again" });
+    }
   };
 
   componentDidMount() {
@@ -17,6 +45,10 @@ class App extends Component {
       this.setState({ user: userDoc });
     }
   }
+
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value, error: "" });
+  };
 
   render() {
     const disable = this.state.password !== this.state.confirm;
