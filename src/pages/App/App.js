8136a01883d3e1d4c0ae44efa90e453372a12d94
Dor <dorzairi@ymail.com>
2021-04-23 18:09:21 -0400

    Pushy push

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,48 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
-import NavBar from '../../components/NavBar/NavBar.jsx'
-import Signup from '../Signup/Signup.jsx'
-import Login from '../Login/Login.jsx'
-import AuthPage from '../AuthPage/AuthPage'
+import NavBar from "../../Components/NavBar/NavBar.jsx";
+import Signup from "../Signup/Signup.jsx";
+import Login from "../Login/Login.jsx";
+import AuthPage from "../AuthPage/AuthPage";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   state = {
-    user:null,
-  }
-  
-  setUserInState = (userDoc) => {
-    this.setState({ user: userDoc})
-  }
+    user: null
+  };
+
+  setUserInState = userDoc => {
+    this.setState({ user: userDoc });
+  };
 
   componentDidMount() {
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem("token");
     if (token) {
       // YOU DO: check expiry!
-      let userDoc = JSON.parse(atob(token.split('.')[1])).user // decode jwt token
-      this.setState({user: userDoc})      
+      let userDoc = JSON.parse(atob(token.split(".")[1])).user; // decode jwt token
+      this.setState({ user: userDoc });
     }
   }
 
   render() {
-    return(
+    return (
       <div>
         <h1>Karmik</h1>
-      <NavBar user={this.state.user} />
+        <NavBar user={this.state.user} />
 
-      <Switch>
+        <Switch>
+          <Route path="/login" render={props => <Login {...props} />} />
 
-      <Route path='/login' render={(props) => (
-        <Login {...props} />
-      )} />
-      
-      <Route path='/signup' render={(props) => (
-        <Signup {...props} 
-        putUserIntoState={this.putUserIntoState}/> 
-      )} />
-      <Redirect to='/' />
-      </Switch>
-
+          <Route
+            path="/signup"
+            render={props => (
+              <Signup {...props} putUserIntoState={this.putUserIntoState} />
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
